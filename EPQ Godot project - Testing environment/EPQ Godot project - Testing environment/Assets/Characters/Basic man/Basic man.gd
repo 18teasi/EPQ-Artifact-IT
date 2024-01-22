abc632dacef3ecd1381c43ff2006684409189d2a
18teasi <144465312+18teasi@users.noreply.github.com>
2024-01-22 13:55:20 +0000

    More weapon switching
    Working on emiting bullets from other positions

--- EPQ Godot project - Testing environment/EPQ Godot project - Testing environment/Assets/Characters/Basic man/Basic man.gd
+++ EPQ Godot project - Testing environment/EPQ Godot project - Testing environment/Assets/Characters/Basic man/Basic man.gd
@@ -2,22 +2,24 @@ extends Node2D
 
 signal player_primary_fire(pos, direction, damage)
 
-const damagevalues = [0,5]
+const damagevalues = [0,5, 2]
 var damage = 0
 var textures = [preload("res://Assets/PNGs/Game Weapon Pistol.png"), 
 preload("res://Assets/PNGs/Overheat MG placeholder.png")]
 var weapon = 0
 const SPEED = 500
 var health = 100
-
+const mgbulletspawns = []
 var can_primary_fire = true
 var bullet_scene = preload("res://Assets/Projectiles/Pistol_Bullet.tscn")
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var nextbulletspawn = $"Pistol Bullet spawn"
+
 
 func on_player_primary_fire():
-	var bulletspawn = nextbulletspawn
+	var bulletspawn = $"Pistol Bullet spawn"
+	if weapon == 1:
+		bulletspawn = $"Pistol Bullet spawn"
+	if weapon == 2:
+		bulletspawn = $"MG Bullet spawns/MG Bullet spawn 1"
 	var player_direction = (get_global_mouse_position()- position).normalized()
 	player_primary_fire.emit(bulletspawn.global_position, player_direction, damage)
 	can_primary_fire = false
@@ -30,7 +32,7 @@ func _process(delta):
 	#movement code
 	var direction = Input.get_vector("MoveLeft","MoveRight","MoveUp","MoveDown")
 	position += direction * SPEED * delta
-	damage = damagevalues[weapon-1]
+	damage = damagevalues[weapon]
 	
 	
 
@@ -42,7 +44,7 @@ func _process(delta):
 	#primary fire input
 	if Input.is_action_pressed("PrimaryFire") and can_primary_fire:
 		if weapon > 0:
-		
+			
 			on_player_primary_fire()
 		
 		#code to handle weapon swapping
@@ -56,8 +58,9 @@ func _process(delta):
 		$GameWeaponSprite.texture = textures[weapon-1]
 		
 	if Input.is_action_just_pressed("Weapon 2"):
-		$"Primary fire cooldown".wait_time = 0.25
+		$"Primary fire cooldown".wait_time = 0.05
 		weapon = 2
+		var bulletspawn = $"MG Bullet spawns"
 		$GameWeaponSprite.texture = textures[weapon-1]
 		
 	
